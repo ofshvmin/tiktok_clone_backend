39debf31fb0c95c65e27c1575fd643ab9cbc3199
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-07-24 09:21:02 -0400

    add error formatting to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser'
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +18,27 @@ async function bootstrap() {
       'apollo-require-preflight',
     ],
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-  })
+  });
+  app.use(cookieParser());
+  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.reduce((accumulator, error) => {
+          accumulator[error.property] = Object.values(error.constraints).join(
+            ', ',
+          )
+          return accumulator
+        }, {})
+
+        throw new BadRequestException(formattedErrors)
+      }
+      //the tutorial has extra commas here at 20:30 out of 6:17:29
+    })
+  )
+
   await app.listen(3000);
 }
 bootstrap();
