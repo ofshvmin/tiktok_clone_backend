e5c50ce1d5b537689262a785e74e7c7e4d02c96f
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-07-24 09:00:18 -0400

    configure nextjs with graphql

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://127.0.0.1:5173',
+    credentials: true,
+    //all headers that clients are allowed to use
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type', 
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  })
   await app.listen(3000);
 }
 bootstrap();
